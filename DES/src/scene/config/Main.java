61f3c18989c074bc476cbaff2f35d2242653a12e
hummai <shining.melon2015@gmail.com>
2020-02-23 21:52:51 +0200

    Added scene design

--- DES/src/scene/config/Main.java
+++ DES/src/scene/config/Main.java
@@ -10,7 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 600, 250));
+        primaryStage.show();
     }
 
 
