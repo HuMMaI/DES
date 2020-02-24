8f0099ec43fe90c22ccceadb7dfd9c465c2c31c5
hummai <shining.melon2015@gmail.com>
2020-02-24 17:41:34 +0200

    Added method for running the algorithm and his initial functional; Renamed fxml file from simple to mainWindow

--- DES/src/scene/config/Controller.java
+++ DES/src/scene/config/Controller.java
@@ -1,4 +1,37 @@
 package scene.config;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private TextArea firstTextArea;
+    @FXML
+    private TextArea secondTextArea;
+    @FXML
+    private Button startButton;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        secondTextArea.setEditable(false);
+    }
+
+    @FXML
+    public void startAlgorithm(ActionEvent actionEvent) {
+        if (firstTextArea.getText().isEmpty()){
+            Alert alert  = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Input field is empty.\nPlease, write some text and try again!");
+            alert.showAndWait();
+        }
+    }
 }
